374b904ce9c19b6a77d077c5ab1c22a548e73c35
Marius Steikūnas <marius@wisemonks.com>
2015-09-30 23:39:12 +0300

    Changed Response class to return itself instead of hash to gain access to additional information.

--- lib/ebayr/response.rb
+++ lib/ebayr/response.rb
@@ -1,18 +1,18 @@
 # -*- encoding : utf-8 -*-
 module Ebayr #:nodoc:
   # A response to an Ebayr::Request.
-  class Response # < Record
+  class Response
     def initialize(request, response)
       ActiveSupport::XmlMini.backend = 'Nokogiri'
 
       @request = request
-      @command = @request.command if @request
       @response = response
-      @body = response.body if @response
-      hash = self.class.from_xml(@body) if @body
-      @response_data = hash["#{@command}Response"] if hash
-      # super(response_data) if response_data
 
+      if response.body
+        hash = Hash.from_xml(response.body)
+        data = hash["#{@request.command}Response"]
+        @response_data = Record.new(data)
+      end
     end
   end
 end
